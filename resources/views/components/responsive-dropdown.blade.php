fd8dcf251e14854079058ff3d6049521fcec7a5c
JanisKaucis <janiskaucis@gmail.com>
2025-01-18 21:34:48 +0200

    add responsive dropdown

---
+++ resources/views/components/responsive-dropdown.blade.php
@@ -0,0 +1,39 @@
+@props(['active', 'align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white'])
+
+@php
+    $alignmentClasses = match ($align) {
+        'left' => 'ltr:origin-top-left rtl:origin-top-right start-0',
+        'top' => 'origin-top',
+        default => 'ltr:origin-top-right rtl:origin-top-left end-0',
+    };
+
+    $width = match ($width) {
+        '48' => 'w-48',
+        default => $width,
+    };
+    $classes = ($active ?? false)
+            ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-indigo-400 text-start text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out'
+            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
+@endphp
+<div {{ $attributes->merge(['class' => $classes]) }}>
+    <div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+        <div @click="open = ! open">
+            {{ $trigger }}
+        </div>
+
+        <div x-show="open"
+             x-transition:enter="transition ease-out duration-200"
+             x-transition:enter-start="opacity-0 scale-95"
+             x-transition:enter-end="opacity-100 scale-100"
+             x-transition:leave="transition ease-in duration-75"
+             x-transition:leave-start="opacity-100 scale-100"
+             x-transition:leave-end="opacity-0 scale-95"
+             class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
+             style="display: none;"
+             @click="open = false">
+            <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+                {{ $content }}
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -102,8 +102,35 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
 
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('blogs')" :active="request()->routeIs('blogs')">
-                {{ __('Blogs') }}
+            <x-responsive-dropdown align="top" width="48" :active="request()->routeIs('blogs') || request()->routeIs('blogs.category')">
+                <x-slot name="trigger">
+                    <button
+                        class="text-base font-medium text-gray-600 flex items-center hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                        <div>{{__('Blogs')}}</div>
+
+                        <div class="ml-1">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 20 20">
+                                <path fill-rule="evenodd"
+                                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                      clip-rule="evenodd"/>
+                            </svg>
+                        </div>
+                    </button>
+                </x-slot>
+                <x-slot name="content">
+                    <x-dropdown-link href="{{ route('blogs') }}">
+                        {{__('All Blogs')}}
+                    </x-dropdown-link>
+                    @foreach($categories as $category)
+                        <x-dropdown-link href="{{ route('blogs.category',['category' => $category->id]) }}">
+                            {{ucfirst($category->name)}}
+                        </x-dropdown-link>
+                    @endforeach
+                </x-slot>
+            </x-responsive-dropdown>
+            <x-responsive-nav-link :href="route('my-blogs')">
+                {{ __('My Blogs') }}
             </x-responsive-nav-link>
         </div>
         @auth()
